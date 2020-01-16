fa43977402a21eca42564cc74dc782505668abd9
liujianyang <876722066@qq.com>
2020-01-16 11:04:39 +0800

    toast 单元测试完成

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="toast" :class="toastClasses" ref="toast">
-        <slot></slot>
-        <template v-if="closeButton">
-            <div class="line" ref="line"></div>
-            <span class="close"  @click="onClickClose">
-                {{closeButton.text}}
-            </span>
-        </template>
-
+    <div class="toastWrap" :class="toastClasses">
+        <div class="toast" ref="toast">
+            <slot></slot>
+            <template v-if="closeButton">
+                <div class="line" ref="line"></div>
+                <span class="close" @click="onClickClose">
+                    {{closeButton.text}}
+                </span>
+            </template>
+        </div>
     </div>
 </template>
 
@@ -18,9 +19,16 @@
             autoClose: {
                 type: [Number, Boolean],
                 default: false,
+                validator(value) {
+                    return value === false || typeof value === 'number'
+                }
             },
             position: {
                 type: String,
+                default: 'top',
+                validator(value) {
+                    return ['top', 'middle', 'bottom'].indexOf(value) >= 0
+                }
             },
             closeButton: {
                 type: Object,
@@ -31,16 +39,16 @@
             this.updateStyles()
         },
         methods: {
-            execAutoClose(){
+            execAutoClose() {
                 if (this.autoClose) {
                     setTimeout(() => {
                         this.close()
                     }, this.autoClose * 1000)
                 }
             },
-            updateStyles(){
+            updateStyles() {
                 this.$nextTick(() => {
-                    if(this.$refs.line){
+                    if (this.$refs.line) {
                         this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
                     }
                 })
@@ -50,9 +58,9 @@
                 this.$emit('beforeClose')
                 this.$destroy()
             },
-            onClickClose(){
+            onClickClose() {
                 this.close()
-                if(this.closeButton && typeof this.closeButton.callback === 'function'){
+                if (this.closeButton && typeof this.closeButton.callback === 'function') {
                     this.closeButton.callback()
                 }
             }
@@ -70,47 +78,96 @@
 <style scoped lang="scss">
     $font-size: 14px;
     $min-height: 40px;
-    .toast {
-        min-height: $min-height;
-        line-height: 1.8;
-        display: flex;
-        align-items: center;
-        color: #fff;
-        font-size: $font-size;
-        background: rgba(0, 0, 0, 0.75);
-        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
-        padding: 0 16px;
-        border-radius: 4px;
+    $border-radius: 4px;
+    $animation-duration: 300ms;
+    @keyframes slide-down {
+        0% {
+            opacity: 0;
+            transform: translateY(-100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-up {
+        0% {
+            opacity: 0;
+            transform: translateY(100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .toastWrap {
         position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
 
         &.position-top {
             top: 0;
-            left: 50%;
-            transform: translateX(-50%);
+
+            .toast {
+                animation: slide-down $animation-duration;
+                border-bottom-left-radius: $border-radius;
+                border-bottom-right-radius: $border-radius;
+            }
         }
 
         &.position-middle {
             top: 50%;
-            left: 50%;
             transform: translateX(-50%) translateY(-50%);
+
+            .toast {
+                animation: fade-in $animation-duration;
+                border-radius: $border-radius;
+            }
         }
 
         &.position-bottom {
             bottom: 0;
-            left: 50%;
-            transform: translateX(-50%);
+
+            .toast {
+                animation: slide-up $animation-duration;
+                border-top-left-radius: $border-radius;
+                border-top-right-radius: $border-radius;
+            }
         }
 
-        .line {
-            height: 100%;
-            border-left: 1px solid #666;
-            margin-left: 16px;
-        }
+        .toast {
+            min-height: $min-height;
+            line-height: 1.8;
+            display: flex;
+            align-items: center;
+            color: #fff;
+            font-size: $font-size;
+            background: rgba(0, 0, 0, 0.75);
+            box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+            padding: 0 16px;
 
-        .close {
-            padding-left: 16px;
-            flex-shrink: 0;
-            cursor: pointer;
+            .line {
+                border-left: 1px solid #666;
+                margin-left: 16px;
+            }
+
+            .close {
+                padding-left: 16px;
+                flex-shrink: 0;
+                cursor: pointer;
+            }
         }
     }
+
 </style>
---
+++ test/toast.test.js
@@ -0,0 +1,60 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Toast from '../src/toast'
+
+
+describe('Toast',() => {
+    it('Toast 存在',() => {
+        expect(Toast).to.be.ok
+    })
+
+    describe('props',() => {
+        let vm
+        afterEach(()=> {
+            vm.$destroy()
+        })
+        it('autoClose 参数',(done) => {
+            const div = document.createElement('div')
+            document.body.appendChild(div)
+            const Constructor = Vue.extend(Toast)
+            vm = new Constructor({
+                propsData: {
+                    autoClose: 1
+                }
+            }).$mount(div)
+            vm.$on('beforeClose',() => {
+                expect(document.body.contains(vm.$el)).to.eq(false)
+                done()
+            })
+
+        })
+        it('position 参数',() => {
+            const Constructor = Vue.extend(Toast)
+            vm = new Constructor({
+                propsData: {
+                    position: 'top'
+                }
+            }).$mount()
+            expect(vm.$el.classList.contains('position-top')).to.eq(true)
+            vm.$destroy()
+        })
+        it('closeButton 参数',() => {
+            const callback = sinon.fake()
+            const Constructor = Vue.extend(Toast)
+            vm = new Constructor({
+                propsData: {
+                    closeButton: {
+                        text:'知道了',
+                        callback,
+                    }
+                }
+            }).$mount()
+            const closeButton = vm.$el.querySelector('.close')
+
+            expect(closeButton.textContent.trim()).to.eq('知道了')
+            closeButton.click()
+            expect(callback).to.have.been.called
+        })
+    })
+
+})
